58d550885c70c67fc4593b56abeaa6c5956c1be5
Chris Reid <chris@raygun6.com>
2019-10-19 17:22:52 -0700

    add elevation to popup

--- 2-mapping/map1.py
+++ 2-mapping/map1.py
@@ -2,16 +2,25 @@ import folium
 import os
 import pandas
 
-map = folium.Map(location=[38.58,-99.09],zoom_start=6,tiles = "Stamen Terrain")
 volcanoes = pandas.read_csv("volcanoes.csv")
 lat = list(volcanoes["LAT"])
 lon = list(volcanoes["LON"])
 elev = list(volcanoes["ELEV"])
 
+def elevation_style(elevation):
+    if elevation < 1000:
+        return "green"
+    elif 1000 <= elevation < 3000:
+        return "orange"
+    else:
+        return "red"
+
+map = folium.Map(location=[38.58,-99.09],zoom_start=6,tiles = "Stamen Terrain")
+
 fg = folium.FeatureGroup(name="My Map")
 
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.Marker(location=[lt, ln],popup=str(el) + " m",icon=folium.Icon(color="green")))
+    fg.add_child(folium.Marker(location=[lt, ln],popup=str(el) + " m",icon=folium.Icon(color=elevation_style(el))))
 
 map.add_child(fg)
 
