66d16714177712721106f4cfabf161507b80d034
Chris Reid <chris@raygun6.com>
2019-10-19 17:01:13 -0700

    add volcano markers

--- 2-mapping/map1.py
+++ 2-mapping/map1.py
@@ -1,13 +1,17 @@
 import folium
 import os
+import pandas
 
 map = folium.Map(location=[38.58,-99.09],zoom_start=6,tiles = "Stamen Terrain")
+volcanoes = pandas.read_csv("volcanoes.csv")
+lat = list(volcanoes["LAT"])
+lon = list(volcanoes["LON"])
 
 fg = folium.FeatureGroup(name="My Map")
 
-for coordinates in [[38.2,-99.1],[37.2,-100.1]]:
-    fg.add_child(folium.Marker(location=coordinates,popup="I'm a marker!",icon=folium.Icon(color="green")))
+for lt, ln in zip(lat, lon):
+    fg.add_child(folium.Marker(location=[lt, ln],popup="I'm a marker!",icon=folium.Icon(color="green")))
 
 map.add_child(fg)
 
-map.save(os.path.expanduser('~/Desktop/map1.html'))
+map.save(os.path.expanduser("~/Desktop/map1.html"))
