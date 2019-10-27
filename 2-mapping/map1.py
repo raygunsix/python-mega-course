5a5153153e167ff47a76a1842d9420317d6d90d3
Chris Reid <chris@raygun6.com>
2019-10-26 17:12:41 -0700

    refactor layers

--- 2-mapping/map1.py
+++ 2-mapping/map1.py
@@ -17,16 +17,21 @@ def elevation_style(elevation):
 
 map = folium.Map(location=[38.58,-99.09],zoom_start=6,tiles = "Stamen Terrain")
 
-fg = folium.FeatureGroup(name="My Map")
+fgv = folium.FeatureGroup(name="Volcanoes")
 
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.CircleMarker(location=[lt, ln],popup=str(el) + " m",
+    fgv.add_child(folium.CircleMarker(location=[lt, ln],popup=str(el) + " m",
     stroke=True,color="grey",fill=True,fill_opacity=0.7,fill_color=elevation_style(el)))
 
-fg.add_child(folium.GeoJson(data=open('world.json','r',encoding='utf-8-sig').read(),
+fgp = folium.FeatureGroup(name="Population")
+
+fgp.add_child(folium.GeoJson(data=open('world.json','r',encoding='utf-8-sig').read(),
 style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000 
     else 'orange' if 100000000 <= x['properties']['POP2005'] < 200000000 else 'red'}))
 
-map.add_child(fg)
+map.add_child(fgv)
+map.add_child(fgp)
+
+map.add_child(folium.LayerControl())
 
 map.save(os.path.expanduser("~/Desktop/map1.html"))
