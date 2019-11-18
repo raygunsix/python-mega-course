8f4df463c10644d173bc42c069770f700b0ea90a
Chris Reid <chris@raygun6.com>
2019-11-17 22:20:19 -0800

    add websites to hosts file

--- 3-website-blocker/website-blocker.py
+++ 3-website-blocker/website-blocker.py
@@ -7,6 +7,14 @@ website_list=["facebook.com", "www.facebook.com"]
 while True:
     if dt(dt.now().year,dt.now().month,dt.now().day,8) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,16):
         print("Working hours...")
+        with open(hosts_path,'r+') as file:
+            content=file.read()
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect + " " + website + "\n")
     else:
         print("Fun hours...")
+
     time.sleep(5)
