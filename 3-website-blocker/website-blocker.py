e3ec02acebb0a09a2d3382a66669e0c86dc30427
Chris Reid <chris@raygun6.com>
2019-11-17 22:35:41 -0800

    remove websites from hosts file during fun hours

--- 3-website-blocker/website-blocker.py
+++ 3-website-blocker/website-blocker.py
@@ -16,5 +16,11 @@ while True:
                     file.write(redirect + " " + website + "\n")
     else:
         print("Fun hours...")
-
+        with open(hosts_path,'r+') as file:
+            content=file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+            file.truncate()
     time.sleep(5)
